7efd640e5f771d157975facdf55e5dde91788a8f
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2013-08-22 13:35:38 +1200

    Display data as well as Gnuplot headers.

--- lib/gnuplot.rb
+++ lib/gnuplot.rb
@@ -99,7 +99,7 @@ module Gnuplot
       @data = []
       @styles = []
       yield self if block_given?
-      puts "writing this to gnuplot:\n" + to_gplot + "\n" if $VERBOSE
+      puts "writing this to gnuplot:\n" + to_gplot + "\n" + store_datasets + "\n" if $VERBOSE
 
       if io    
         io << to_gplot
